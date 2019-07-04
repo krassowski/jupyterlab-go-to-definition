4bbcaa6664581ac996756170201a6283c11bb9cf
krassowski <krassowski.michal@gmail.com>
2019-07-04 01:12:41 +0100

    Fix tests using the idea of @lukejagodzinski from https://github.com/facebook/jest/issues/6229

--- jest.config.js
+++ jest.config.js
@@ -2,17 +2,20 @@ const func = require('@jupyterlab/testutils/lib/jest-config');
 const upstream = func('jupyterlab_go_to_definition', __dirname);
 
 const reuseFromUpstream = [
-    'preset',
     'moduleNameMapper',
     'setupFilesAfterEnv',
     'setupFiles',
-    'testPathIgnorePatterns',
     'moduleFileExtensions',
 ];
 
 let local = {
   globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
   testRegex: `.*\.spec\.tsx?$`,
+  transform: {
+    "\\.(ts|tsx)?$": "ts-jest",
+    "\\.(js|jsx)?$": "./transform.js"
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(@jupyterlab/.*)/)"]
 };
 
 for(option of reuseFromUpstream) {
