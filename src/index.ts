49d39ffe409f722125039d5f06eb4199315270bd
krassowski <krassowski.michal@gmail.com>
2019-08-17 06:03:19 +0100

    Integrate LSP code completion with JupyterLab (WIP)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ICommandPalette } from "@jupyterlab/apputils";
 import { INotebookTracker } from "@jupyterlab/notebook";
 import { CodeMirrorEditor } from '@jupyterlab/codemirror';
 import { FileEditor, IEditorTracker } from '@jupyterlab/fileeditor';
-import { ISettingRegistry } from '@jupyterlab/coreutils';
+import { DataConnector, ISettingRegistry } from '@jupyterlab/coreutils';
 import { IDocumentManager } from '@jupyterlab/docmanager';
 
 import { FileEditorJumper } from "./jumpers/fileeditor";
@@ -12,6 +12,7 @@ import { NotebookJumper } from "./jumpers/notebook";
 import { CodeMirrorExtension } from "./editors/codemirror";
 import { KeyModifier } from "./editors/editor";
 import { JumpHistory } from "./history";
+import { ReadonlyJSONObject } from '@phosphor/coreutils';
 
 import 'codemirror/addon/hint/show-hint.css';
 import 'codemirror/addon/hint/show-hint';
@@ -20,7 +21,277 @@ import 'lsp-editor-adapter/lib/codemirror-lsp.css';
 import { LspWsConnection, CodeMirrorAdapter, IPosition } from 'lsp-editor-adapter';
 import { IDocumentWidget } from "@jupyterlab/docregistry";
 import { CodeEditor } from "@jupyterlab/codeeditor";
+import { CompletionHandler, ICompletionManager, ContextConnector } from '@jupyterlab/completer';
+import * as lsProtocol from "vscode-languageserver-protocol";
 
+/**
+ * A LSP connector for completion handlers.
+ */
+export class LSPConnector extends DataConnector<
+  CompletionHandler.IReply,
+  void,
+  CompletionHandler.IRequest
+> {
+  /**
+   * Create a new LSP connector for completion requests.
+   *
+   * @param options - The instantiation options for the LSP connector.
+   */
+  constructor(options: LSPConnector.IOptions) {
+    super();
+    this._editor = options.editor;
+    this._connection = options.connection;
+    this._completion_characters = this._connection.getLanguageCompletionCharacters();
+    this._context_connector = new ContextConnector({editor: options.editor})
+  }
+
+  /**
+   * Fetch completion requests.
+   *
+   * @param request - The completion request text and details.
+   */
+  fetch(
+    request: CompletionHandler.IRequest
+  ): Promise<CompletionHandler.IReply> {
+
+    try {
+      if (this._completion_characters === undefined)
+        this._completion_characters = this._connection.getLanguageCompletionCharacters();
+
+      return Private.hint(this._editor, this._connection, this._completion_characters).catch((e) => {
+        console.log(e);
+        return this._context_connector.fetch(request)
+        }
+      )
+    }
+    catch (e) {
+      return this._context_connector.fetch(request)
+    }
+  }
+
+  private readonly _editor: CodeEditor.IEditor;
+  private readonly _connection: LspWsConnection;
+  private _completion_characters: Array<string>;
+  private _context_connector: ContextConnector;
+}
+
+/**
+ * A namespace for LSP connector statics.
+ */
+export namespace LSPConnector {
+  /**
+   * The instantiation options for cell completion handlers.
+   */
+  export interface IOptions {
+    /**
+     * The session used by the LSP connector.
+     */
+    editor: CodeEditor.IEditor;
+    connection: LspWsConnection;
+  }
+}
+
+function convert_position(position: CodeEditor.IPosition): IPosition {
+  return {
+    line: position.line,
+    ch: position.column
+  }
+}
+
+async function sleep(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {resolve()}, timeout);
+  })
+}
+
+function once_set(x: any, max_retrials: number=35) {
+  return new Promise(async (resolve, reject) => {
+    let i = 0;
+    while (x.set !== true) {
+      i += 1;
+      if(i > max_retrials) {
+        reject('Too many retrials');
+        break
+      }
+      console.log('waiting');
+      await sleep(50)
+    }
+    resolve(x.value);
+  });
+}
+
+interface IItemType extends ReadonlyJSONObject {
+  // the item value
+  text: string
+  // the item type
+  type: string
+}
+
+namespace CompletionItemKind {
+	export const Text = 1;
+	export const Method = 2;
+	export const Function = 3;
+	export const Constructor = 4;
+	export const Field = 5;
+	export const Variable = 6;
+	export const Class = 7;
+	export const Interface = 8;
+	export const Module = 9;
+	export const Property = 10;
+	export const Unit = 11;
+	export const Value = 12;
+	export const Enum = 13;
+	export const Keyword = 14;
+	export const Snippet = 15;
+	export const Color = 16;
+	export const File = 17;
+	export const Reference = 18;
+	export const Folder = 19;
+	export const EnumMember = 20;
+	export const Constant = 21;
+	export const Struct = 22;
+	export const Event = 23;
+	export const Operator = 24;
+	export const TypeParameter = 25;
+}
+
+const itemKinds: Record<number, string> = {};
+for(let key of Object.keys(CompletionItemKind)) {
+  // @ts-ignore
+  itemKinds[CompletionItemKind[key]] = key;
+}
+
+/**
+ * A namespace for Private functionality.
+ */
+namespace Private {
+  /**
+   * Get a list of completion hints from a tokenization
+   * of the editor.
+   */
+  export async function hint(
+    editor: CodeEditor.IEditor,
+    connection: LspWsConnection,
+    completion_characters: Array<string>
+  ): Promise<CompletionHandler.IReply> {
+    // Find the token at the cursor
+    const cursor = editor.getCursorPosition();
+    const token = editor.getTokenForPosition(cursor);
+
+    const start = editor.getPositionAt(token.offset);
+    const end = editor.getPositionAt(token.offset + token.value.length);
+
+    // const signatureCharacters = connection.getLanguageSignatureCharacters();
+
+    const typedCharacter = token.value[cursor.column - start.column - 1];
+
+    // without sendChange we (sometimes) get outdated suggestions
+    connection.sendChange();
+
+    //let request_completion: Function;
+    let event: string;
+
+    // nope - do not do this; we need to get the signature (yes)
+    // but only in order to bump the priority of the parameters!
+    // unfortunately there is no abstraction of scores exposed
+    // to the matches...
+    // TODO: pull request which:
+    //  - changes the text+metadata into list of objects obeying an interface
+    //  - the interface allows to pass an optional score (number)
+    //  - if the score is set, the _filter does not recalculate it (https://github.com/jupyterlab/jupyterlab/blob/3592afd328116a588e3307b4cdd9bcabc7fe92bb/packages/completer/src/model.ts#L373
+    //  - the interface also allows to provide documentation, which is not used by default
+    //  - but the renderer is modified so that it can be customized (making use of documentation)
+
+    //if (signatureCharacters.indexOf(typedCharacter) !== -1) {
+    //  // @ts-ignore
+    //  request_completion = connection.getSignatureHelp.bind(this);
+    //  event = 'signature'
+    //} else {
+      // @ts-ignore
+      //request_completion = connection.getCompletion.bind(this);
+      event = 'completion';
+    //}
+    //*/
+
+    // todo: regexpr?
+    //if(completion_characters.indexOf(typedCharacter) === -1)
+    //  return
+
+    // in Node v11.13.0, once() was added which would enable using native promises here:
+    // https://nodejs.org/api/events.html#events_events_once_emitter_name
+    // but it has not been implemented in 'events':
+    // https://nodejs.org/api/events.html
+    // yet (as for today they match Node.js v10.1)
+    // There is an issue:
+    // https://github.com/Gozala/events/issues/63
+
+    connection.getCompletion(
+      convert_position(cursor),
+      {
+        start: convert_position(start),
+        end: convert_position(end),
+        text: token.value
+      },
+      //completion_characters.find((c) => c === typedCharacter)
+      typedCharacter,
+      //lsProtocol.CompletionTriggerKind.TriggerCharacter,
+    );
+    let result: any = {set: false};
+    connection.once(event, (args: any) => {
+      result.value = args;
+      result.set = true;
+      return args
+    });
+    await once_set(result);
+
+    console.log(result);
+
+    let matches: Array<string> = [];
+    const types: Array<IItemType> = [];
+
+    let no_prefix = true;
+    for(let match of result.value) {
+      // there are more interesting things to be extracted and passed to the metadata:
+      // detail: "__main__"
+      // documentation: "mean(data)↵↵Return the sample arithmetic mean of data.↵↵>>> mean([1, 2, 3, 4, 4])↵2.8↵↵>>> from fractions import Fraction as F↵>>> mean([F(3, 7), F(1, 21), F(5, 3), F(1, 3)])↵Fraction(13, 21)↵↵>>> from decimal import Decimal as D↵>>> mean([D("0.5"), D("0.75"), D("0.625"), D("0.375")])↵Decimal('0.5625')↵↵If ``data`` is empty, StatisticsError will be raised."
+      // insertText: "mean"
+      // kind: 3
+      // label: "mean(data)"
+      // sortText: "amean"
+      let text = match.insertText ? match.insertText : match.label;
+      if (text.toLowerCase().startsWith(token.value.toLowerCase())) {
+        no_prefix = false;
+      }
+
+      matches.push(text);
+      types.push({
+        text: text,
+        type: match.kind ? itemKinds[match.kind] : ''
+      })
+    }
+    console.log(matches)
+    console.log(types)
+
+    return {
+      // note in the ContextCompleter it was:
+      // start: token.offset,
+      // end: token.offset + token.value.length,
+      // which does not work with "from statistics import <tab>" as the last token ends at "t" of "import",
+      // so the completer would append "mean" as "from statistics importmean" (without space!);
+      // (in such a case the typedCharacters is undefined as we are out of range)
+      // a different workaround would be to prepend the token.value prefix:
+      // text = token.value + text;
+      // but it did not work for "from statistics <tab>" and lead to "from statisticsimport" (no space)
+      start: no_prefix ? token.offset + token.value.length : token.offset,
+      end: token.offset + token.value.length,
+      matches: matches,
+      metadata: {
+        _jupyter_types_experimental: types
+      }
+    };
+  }
+
+}
 
 class CodeMirrorAdapterExtension extends CodeMirrorAdapter {
 
@@ -28,8 +299,12 @@ class CodeMirrorAdapterExtension extends CodeMirrorAdapter {
     // @ts-ignore
     this._removeTooltip();
 
-    // do not handle GoTo actions here
+    // do NOT handle GoTo actions here
   }
+
+  public handleCompletion(completions: lsProtocol.CompletionItem[]) {
+    // do NOT handle completion here
+  };
 
 }
 
@@ -43,7 +318,7 @@ class FileEditorAdapter {
   connection: LspWsConnection;
   app: JupyterFrontEnd;
 
-  constructor(editor_widget: IDocumentWidget<FileEditor>, jumper: FileEditorJumper, app: JupyterFrontEnd) {
+  constructor(editor_widget: IDocumentWidget<FileEditor>, jumper: FileEditorJumper, app: JupyterFrontEnd, completion_manager: ICompletionManager) {
     this.widget = editor_widget;
     this.editor = editor_widget.content;
 
@@ -100,6 +375,7 @@ class FileEditorAdapter {
       if (uri.startsWith('file://'))
         uri = uri.slice(7);
 
+      console.log(uri);
       jumper.global_jump({
         // TODO: there are many files which are not symlinks
         uri: '.lsp_symlink/' + uri,
@@ -108,6 +384,16 @@ class FileEditorAdapter {
         column: column
       }, true);
 
+    });
+
+    const connector = new LSPConnector({
+      editor: this.editor.editor,
+      connection: this.connection
+    });
+    completion_manager.register({
+      connector,
+      editor: this.editor.editor,
+      parent: editor_widget,
     });
 
     console.log('Connected adapter');
@@ -151,14 +437,15 @@ const file_jumpers: Map<string, FileEditorJumper> = new Map();
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: '@krassowski/jupyterlab_go_to_definition:plugin',
-  requires: [IEditorTracker, INotebookTracker, ISettingRegistry, ICommandPalette, IDocumentManager],
+  requires: [IEditorTracker, INotebookTracker, ISettingRegistry, ICommandPalette, IDocumentManager, ICompletionManager],
   activate: (
     app: JupyterFrontEnd,
     fileEditorTracker: IEditorTracker,
     notebookTracker: INotebookTracker,
     settingRegistry: ISettingRegistry,
     palette: ICommandPalette,
-    documentManager: IDocumentManager
+    documentManager: IDocumentManager,
+    completion_manager: ICompletionManager
   ) => {
 
     let jump_history = new JumpHistory();
@@ -182,7 +469,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
         let jumper = new FileEditorJumper(widget, jump_history, documentManager);
         let extension = new CodeMirrorExtension(fileEditor.editor, jumper);
         file_jumpers.set(fileEditor.id, jumper);
-        let adapter = new FileEditorAdapter(widget, jumper, app);
+        let adapter = new FileEditorAdapter(widget, jumper, app, completion_manager);
         file_editor_adapters.set(fileEditor.id, adapter);
         extension.connect();
       }
